52f42ff219845ed40b10b641c170f32742c93b38
lvnan888 <2038154109@qq.com>
2026-06-06 21:39:30 +0800

    Site updated: 2026-06-06 21:39:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/06/hello-world/"];function toRandomPost(){
+var posts=["2026/06/06/hello-world/","2026/06/06/My-New-Post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
