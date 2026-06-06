8a09dab4a14c8b98f888a83271109f625ab53830
lvnan888 <2038154109@qq.com>
2026-06-07 00:07:27 +0800

    Site updated: 2026-06-07 00:07:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/06/My-New-Post/","2026/06/06/hello-world/","2026/06/06/naiwa/"];function toRandomPost(){
+var posts=["2026/06/06/My-New-Post/","2026/06/06/naiwa/","2026/06/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
